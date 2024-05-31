f14859a6c5c3e2434ef18e03055120850305035d
Miracle Adah <75673864+Dickson2020@users.noreply.github.com>
2024-05-31 22:00:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.get("/getUser", async (req, res) => {
 });
 
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
