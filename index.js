9cbe02460451619bdb693f763d7af91babe23aac
Miracle Adah <75673864+Dickson2020@users.noreply.github.com>
2024-05-31 23:15:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,25 +6,25 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my API!");
+  res.json("Welcome to my API!");
 });
 
 
 app.post("/createuser", async (req, res) => {
   const userData = req.body;
   const user = await createUser(userData);
-  res.send(user);
+  res.json(user);
 });
 
 app.get("/getusers", async (req, res) => {
   const users = await getUsers();
-  res.send(users);
+  res.json(users);
 });
 
 app.get("/getuser", async (req, res) => {
   const userId = req.query.userId;
   const user = await getUser(userId);
-  res.send(user);
+  res.json(user);
 });
 
 
@@ -33,4 +33,3 @@ const port = 5000;
 app.listen(port, () => console.log(`Server running on ${port}`));
 
 
-module.exports = app;
